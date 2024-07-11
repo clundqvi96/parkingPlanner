4c3a88217ec41f61af0775d64d3485e67770c274
Lundqvist <casper.lundqvist@sogeti.com>
2024-07-11 13:46:51 +0200

    delete booking now works

--- src/app/services/booking.service.ts
+++ src/app/services/booking.service.ts
@@ -23,9 +23,16 @@ export class BookingService {
   }
 
   // Add a method to create a booking
-createBooking(bookingData: { parkingNumber: string; date: string; name: string }): Observable<any> {
+  createBooking(bookingData: { parkingNumber: string; date: string; name: string }): Observable<any> {
   return this.http.post(`${this.baseUrl}`, bookingData);
-}
+  }
 
-  // Lägg till fler metoder för att hantera andra endpoints, som att skapa en bokning, uppdatera, etc.
+  // Add a method to delete a booking the url should be `${this.baseUrl}/<employee.name>/<date>/<parkingNumber>`
+  deleteBooking(bookingData: { parkingNumber: string; date: string; name: string }): Observable<any> {
+    // Använd encodeURIComponent för att hantera namn med mellanslag eller andra specialtecken korrekt
+    const encodedName = encodeURIComponent(bookingData.name);
+    const url = `${this.baseUrl}/${encodedName}/${bookingData.date}/${bookingData.parkingNumber}`;
+    console.log('url:', bookingData, url);
+    return this.http.delete(url);
+  }
 }
